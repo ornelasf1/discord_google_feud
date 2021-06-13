ce120afaed963a817f391de4295bdb9618fb422d
Fabian <ornelasf1@gmail.com>
2021-06-13 15:57:23 -0500

    Fixed wrong script location

--- scripts/schedule_reset_contributions.sh
+++ scripts/schedule_reset_contributions.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 #write out current crontab
 sudo crontab -l > mycron
 #echo new cron into cron file
-echo "0 6 * * * ./$SCRIPT_DIR/reset_contributions.sh" >> mycron
+echo "0 6 * * * $SCRIPT_DIR/reset_contributions.sh" >> mycron
 #install new cron file
 sudo crontab mycron
 rm mycron

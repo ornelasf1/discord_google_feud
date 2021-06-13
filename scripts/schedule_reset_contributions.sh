b75230b1d65eb4a468ce08d909ae12d299366bbd
Fabian <ornelasf1@gmail.com>
2021-06-13 15:54:25 -0500

    Updated crontab script

--- scripts/schedule_reset_contributions.sh
+++ scripts/schedule_reset_contributions.sh
@@ -3,9 +3,13 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 #write out current crontab
-crontab -l > mycron
+sudo crontab -l > mycron
 #echo new cron into cron file
 echo "0 6 * * * ./$SCRIPT_DIR/reset_contributions.sh" >> mycron
 #install new cron file
 sudo crontab mycron
 rm mycron
+
+echo 'New crontab:'
+echo
+sudo crontab -l
